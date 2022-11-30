139912fb114de2cbcac3bc5c5e74a6eed7d08a0e
AdamLyun <adamlyun@gmail.com>
2022-11-30 13:13:23 -0500

    BON appitite part 4

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,5 +1,31 @@
 import "../styles/style.css";
 import { menu } from "./menu";
-
-
 console.log(menu);
+
+document.querySelector(".btn").addEventListener("click", function(){
+    if(    document.body.classList.contains("lightmode")){  
+     document.body.classList.add("darkmode");
+     document.body.classList.remove("lightmode");
+     document.body.style.color = 'black';
+    }else{
+        document.body.classList.add("lightmode");
+        document.body.classList.remove("darkmode");
+        document.body.style.color = 'white';
+    };
+})
+;
+document.querySelector(".btn2").addEventListener("click", function(){
+    if(    document.body.classList.contains("lightmode")){  
+     document.body.classList.add("darkmode");
+     document.body.classList.remove("lightmode");
+     document.body.style.color = 'green';
+    }else{
+        document.body.classList.add("lightmode");
+        document.body.classList.remove("darkmode");
+        document.body.style.color = 'yellow';
+    };
+})
+;
+
+
+

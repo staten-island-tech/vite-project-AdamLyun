bf9a5787a5578826952970d151b75ee44ccda3d5
AdamLyun <adamlyun@gmail.com>
2022-12-02 13:12:32 -0500

    bon part 6

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -7,21 +7,22 @@ DOMSelectors.button.addEventListener("click", function () {
   if (document.body.classList.contains("lightmode")) {
     document.body.classList.add("darkmode");
     document.body.classList.remove("lightmode");
-    document.body.style.color = "black";
+    document.body.style.color = "white";
   } else {
     document.body.classList.add("lightmode");
     document.body.classList.remove("darkmode");
-    document.body.style.color = "white";
+    document.body.style.color = "black";
   }
 });
 
 champion.forEach(champ => { DOMSelectors.box.insertAdjacentHTML(
     "beforeend",
-    `<div class="output">
-    <h3> ${champ.id}</h3>
-    <img src=".png" alt="No Image???">
-     <h3></h3
-    </div>`
+    `
+
+    <div class="inner">  
+    <p>${champ.id} </p>
+    </div>
+`
   );
     
 });

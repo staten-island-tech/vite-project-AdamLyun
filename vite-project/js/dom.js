879fd46a6df62952ef3ff931632948aaf7020ecf
AdamLyun <adamlyun@gmail.com>
2022-12-08 13:16:52 -0500

    bon part 10

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -1,6 +1,20 @@
 const DOMSelectors = {
     button: document.getElementById("btn1"),
     button2: document.getElementById("btn2"),
+    button3: document.getElementById("btn3"),
+    button4: document.getElementById("btn4"),
     box: document.getElementById("container-box"),
   };
   export {DOMSelectors};
+
+  DOMSelectors.button.addEventListener("click", function () {
+    if (document.body.classList.contains("lightmode")) {
+      document.body.classList.add("darkmode");
+      document.body.classList.remove("lightmode");
+      document.body.style.color = "white";
+    } else {
+      document.body.classList.add("lightmode");
+      document.body.classList.remove("darkmode");
+      document.body.style.color = "black";
+    }
+  });
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -3,46 +3,70 @@ import { champion } from "./menu";
 console.log(champion);
 import { DOMSelectors } from "./dom";
 
-DOMSelectors.button.addEventListener("click", function () {
-  if (document.body.classList.contains("lightmode")) {
-    document.body.classList.add("darkmode");
-    document.body.classList.remove("lightmode");
-    document.body.style.color = "white";
-  } else {
-    document.body.classList.add("lightmode");
-    document.body.classList.remove("darkmode");
-    document.body.style.color = "black";
-  }
-});
-
-createcards()
-
-function createcards(){
-champion.forEach(champ => { DOMSelectors.box.insertAdjacentHTML(
-    "beforeend",
+const commands = {
+  createcards: function(){
+    champion.forEach(champ => { DOMSelectors.box.insertAdjacentHTML(
+        "beforeend",
+        `
+        <div class="inner">   
+        <p>${champ.name} </p>
+        <img src="${champ.id}.png" alt="No Image???">
+        <p>${champ.title}</p>
+        </div>
     `
-    <div class="inner">   
-    <p>${champ.name} </p>
-    <img src="${champ.id}.png" alt="No Image???">
-    <p>${champ.title}</p>
-    </div>
-`
-  );
-});
-};
+      );
+    });
+    },
+  filterMarksman: function(){
+    champion.filter((champion) => champion.tags.includes(`Marksman`))
+    .forEach(champ => { DOMSelectors.box.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="inner">  
+      <p>${champ.name} </p>
+      <img src="${champ.id}.png" alt="No Image???">
+      <p>${champ.title}</p>
+      </div>
+  `
+    );
+    });
+    
+  },
+  filterMage: function(){
+    champion.filter((champion) => champion.tags.includes(`Mage`))
+    .forEach(champ => { DOMSelectors.box.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="inner">  
+      <p>${champ.name} </p>
+      <img src="${champ.id}.png" alt="No Image???">
+      <p>${champ.title}</p>
+      </div>
+  `
+    );
+    });
+    
+  },
+  filterTank: function(){
+    champion.filter((champion) => champion.tags.includes(`Tank`))
+    .forEach(champ => { DOMSelectors.box.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="inner">  
+      <p>${champ.name} </p>
+      <img src="${champ.id}.png" alt="No Image???">
+      <p>${champ.title}</p>
+      </div>
+  `
+    );
+    });
+    
+  },
 
-DOMSelectors.button2.addEventListener("click", function(){
-  champion.filter((champion) => champion.tags.includes(`Marksman`))
-  .forEach(champ => { DOMSelectors.box.insertAdjacentHTML(
-    "beforeend",
-    `
-    <div class="inner">  
-    <p>${champ.name} </p>
-    <img src="${champ.id}.png" alt="No Image???">
-    <p>${champ.title}</p>
-    </div>
-`
-  );
-  });
-  
-}) ; 
+}
+
+
+
+DOMSelectors.button2.addEventListener("click", commands.filterMarksman) 
+DOMSelectors.button3.addEventListener("click", commands.filterMage)
+DOMSelectors.button4.addEventListener("click", commands.filterTank)
